3450cd4d98069677bc6e5102e88b3f27c70cc63e
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-03-31 13:52:37 +0200

    Add apfel formula

---
+++ Formula/apfel.rb
@@ -0,0 +1,27 @@
+class Apfel < Formula
+  desc "On-device Apple FoundationModels CLI and OpenAI-compatible server"
+  homepage "https://github.com/Arthur-Ficial/apfel"
+  url "https://github.com/Arthur-Ficial/apfel/releases/download/v0.6.4/apfel-0.6.4-arm64-macos.tar.gz"
+  sha256 "50f69c547d793e5da823ead182ca20f25591cd28bb1fdb199eafa8d3757c386c"
+  license "MIT"
+
+  def install
+    odie "apfel requires Apple Silicon." unless Hardware::CPU.arm?
+    odie "apfel requires macOS 26.4 or newer." if MacOS.version < Version.new("26.4")
+
+    bin.install "apfel"
+  end
+
+  def caveats
+    <<~EOS
+      apfel runs entirely on-device and requires Apple Intelligence to be enabled.
+
+      Check model availability with:
+        apfel --model-info
+    EOS
+  end
+
+  test do
+    assert_match "apfel v0.6.4", shell_output("#{bin}/apfel --version")
+  end
+end
