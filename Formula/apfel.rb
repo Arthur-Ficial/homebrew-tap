9abdbe353c01a92fbe474b8c9cff3a0f9a1c418f
Arthur Ficial <arthur@users.noreply.github.com>
2026-04-02 12:46:19 +0200

    apfel v0.6.13

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -1,8 +1,8 @@
 class Apfel < Formula
   desc "On-device Apple FoundationModels CLI and OpenAI-compatible server"
   homepage "https://github.com/Arthur-Ficial/apfel"
-  url "https://github.com/Arthur-Ficial/apfel/releases/download/v0.6.11/apfel-0.6.11-arm64-macos.tar.gz"
-  sha256 "cbf1c7be6d745f0393e64e95a2d1c64865c98722d258655e6e4d10e280212ab4"
+  url "https://github.com/Arthur-Ficial/apfel/releases/download/v0.6.13/apfel-0.6.13-arm64-macos.tar.gz"
+  sha256 "1a927fe382f11004704dc162e1ab16c4c36403d0a281b9cb3a5a5df2148e3d26"
   license "MIT"
 
   def install
@@ -21,6 +21,6 @@ class Apfel < Formula
   end
 
   test do
-    assert_match "apfel v0.6.11", shell_output("#{bin}/apfel --version")
+    assert_match "apfel v0.6.13", shell_output("#{bin}/apfel --version")
   end
 end
