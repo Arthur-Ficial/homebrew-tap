6f4dc5ebff1f046710a606fa3fe5776cce534fd6
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-04-05 11:30:33 +0200

    apfel v0.8.4

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -1,8 +1,8 @@
 class Apfel < Formula
   desc "On-device Apple FoundationModels CLI and OpenAI-compatible server"
   homepage "https://github.com/Arthur-Ficial/apfel"
-  url "https://github.com/Arthur-Ficial/apfel/releases/download/v0.8.3/apfel-0.8.3-arm64-macos.tar.gz"
-  sha256 "05ae7ec40db859a5f65c52fd2af1199ef987d3c298ef19c51ab68f39bce94120"
+  url "https://github.com/Arthur-Ficial/apfel/releases/download/v0.8.4/apfel-0.8.4-arm64-macos.tar.gz"
+  sha256 "89ab3d19e05c6cd8c4c1df6d838686000fd6e2b5fcc5384557c70c7a62511872"
   license "MIT"
 
   def install
@@ -21,6 +21,6 @@ class Apfel < Formula
   end
 
   test do
-    assert_match "apfel v0.8.3", shell_output("#{bin}/apfel --version")
+    assert_match "apfel v0.8.4", shell_output("#{bin}/apfel --version")
   end
 end
