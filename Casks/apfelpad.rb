b74b63ca906755ea3032de9afa0eb80a534f5daa
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-04-12 03:05:12 +0200

    cask: update apfelpad to v0.5.1

--- Casks/apfelpad.rb
+++ Casks/apfelpad.rb
@@ -1,6 +1,6 @@
 cask "apfelpad" do
-  version "0.5.0"
-  sha256 "0254a1aeac4d2eed3ee964eeb94f475e5578a01cc8ce1fce9b59a64826b2ba39"
+  version "0.5.1"
+  sha256 "dff202603fe08f2fc45a905158351e4e6e55cd406ff7d05615fbc37514ef25db"
 
   url "https://github.com/Arthur-Ficial/apfelpad/releases/download/v#{version}/apfelpad-macos-arm64.zip"
   name "apfelpad"
