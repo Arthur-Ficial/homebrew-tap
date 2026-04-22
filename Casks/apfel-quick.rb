ca616d0750d576a5a68d37c86c0eaa4431de623e
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-04-23 01:20:23 +0200

    cask: update apfel-quick to v1.0.6

--- Casks/apfel-quick.rb
+++ Casks/apfel-quick.rb
@@ -1,6 +1,6 @@
 cask "apfel-quick" do
-  version "1.0.5"
-  sha256 "1e4e185d35ee129c1232b45bf3fead53581fa5d1207f73253f5f2e1e09a139f0"
+  version "1.0.6"
+  sha256 "b64f5ce7198fddde39462037b20f2d18fe4a7b846703ca9b5f61d0a931b7739d"
 
   url "https://github.com/Arthur-Ficial/apfel-quick/releases/download/v#{version}/apfel-quick-macos-arm64.zip"
   name "apfel-quick"
