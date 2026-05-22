c69e6687e0a2c9a047bf744ccb41608a7ed2727b
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-05-22 17:27:51 +0200

    cask: update bgbgone-app to v0.1.1

--- Casks/bgbgone-app.rb
+++ Casks/bgbgone-app.rb
@@ -1,6 +1,6 @@
 cask "bgbgone-app" do
-  version "0.1.0"
-  sha256 "f28e1575f61aa372a3dd2f07db22a7fcc50b7e96f0c81ae2ec86e17536d34d5b"
+  version "0.1.1"
+  sha256 "1300f70090f9d96cba3d3a2445f6917b40382ecabd7c5ffb12b6b7b706eac2ad"
 
   url "https://github.com/Arthur-Ficial/bgbgone.app/releases/download/v#{version}/bgbgone-app-v#{version}-macos-arm64.zip"
   name "bgbgone"
