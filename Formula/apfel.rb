20aa5d3bf9343c8d0a8365dd380d759fb59f2149
Arthur Ficial <arti.ficial@fullstackoptimization.com>
2026-06-09 23:19:38 +0200

    apfel v1.5.5

--- Formula/apfel.rb
+++ Formula/apfel.rb
@@ -1,8 +1,8 @@
 class Apfel < Formula
   desc "On-device Apple FoundationModels CLI and OpenAI-compatible server"
   homepage "https://github.com/Arthur-Ficial/apfel"
-  url "https://github.com/Arthur-Ficial/apfel/releases/download/v1.5.4/apfel-1.5.4-arm64-macos.tar.gz"
-  sha256 "3af0834d0aa8e34380c417b7240eb7256611a7d52c3bda4529c19dcb418baccb"
+  url "https://github.com/Arthur-Ficial/apfel/releases/download/v1.5.5/apfel-1.5.5-arm64-macos.tar.gz"
+  sha256 "5d35d8684d18d693bbebe09b4320dc03ce106105e168cd24a38070cd182d5e6a"
   license "MIT"
 
   depends_on arch: :arm64
